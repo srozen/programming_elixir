52a1d3980681ecc01ac9832a9c015d179ab4d27b
Samuel Monroe <spat.monroe@gmail.com>
2018-11-02 21:36:02 +0900

    Prepare fetch from github

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -37,4 +39,14 @@ defmodule Issues.CLI do
     :help
   end
 
+  defp process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [count | #{@default_count}]
+    """
+    System.halt(0)
+  end
+
+  defp process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
 end
