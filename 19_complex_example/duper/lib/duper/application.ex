07bfc9fba7777cb05ebad6c6dea02849ccc570e4
Samuel Monroe <spat.monroe@gmail.com>
2018-11-19 12:05:06 +0900

    Results server for Duper

---
+++ 19_complex_example/duper/lib/duper/application.ex
@@ -0,0 +1,19 @@
+defmodule Duper.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      Duper.Results,
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
