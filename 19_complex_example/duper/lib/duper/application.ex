f3604ef3871c8bbe48328411d0e02c79b14e2349
Samuel Monroe <spat.monroe@gmail.com>
2018-11-19 13:07:23 +0900

    PathFinder Server

--- 19_complex_example/duper/lib/duper/application.ex
+++ 19_complex_example/duper/lib/duper/application.ex
@@ -9,6 +9,7 @@ defmodule Duper.Application do
     # List all child processes to be supervised
     children = [
       Duper.Results,
+      {Duper.PathFinder, "."},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
