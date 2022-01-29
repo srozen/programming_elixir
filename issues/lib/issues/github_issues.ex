84a7b79ad5cb7f9c6b5763816b2c6848a90b653a
Samuel Monroe <spat.monroe@gmail.com>
2022-01-29 09:54:18 +0100

    Logging and escript

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,8 +1,12 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @github_url Application.fetch_env!(:issues, :github_url)
   @user_agent [{"User-agent", "Elixir spat.monroe@gmail.com"}]
 
   def fetch(user, project) do
+    Logger.info(fn  -> "Fetching #{user}'s project #{project}" end)
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -12,7 +16,22 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
+  @spec handle_response(
+          {any,
+           %{
+             :body =>
+               binary
+               | maybe_improper_list(
+                   binary | maybe_improper_list(any, binary | []) | byte,
+                   binary | []
+                 ),
+             :status_code => any,
+             optional(any) => any
+           }}
+        ) :: {:error, any} | {:ok, any}
   def handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code =#{status_code}")
+    Logger.debug(fn  -> inspect(body) end)
     {
       status_code |> check_for_error(),
       body |> Jason.decode!()
--- issues/lib/issues/table_formatter.ex
+++ issues/lib/issues/table_formatter.ex
@@ -2,8 +2,8 @@ defmodule Issues.TableFormatter do
   import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1]
 
   def print_table_for_columns(rows, headers) do
-    with data_by_columns = split_into_columns(rows, headers)
-         column_widths   = widths_of(data_by_columns)
+    with data_by_columns = split_into_columns(rows, headers),
+         column_widths   = widths_of(data_by_columns),
          format          = format_for(column_widths)
     do
       puts_one_line_in_columns(headers, format)
@@ -26,8 +26,21 @@ defmodule Issues.TableFormatter do
   end
 
   def format_for(column_widths) do
-    map_join(column_widths, " / ", fn width -> "~-#{width}s" end) <> "~n"
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
   end
 
-  def separator()
+  def separator(column_widths) do
+    map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
+  end
+
+  def puts_in_columns(data_by_columns, format) do
+    data_by_columns
+    |> List.zip
+    |> map(&Tuple.to_list/1)
+    |> each(&puts_one_line_in_columns(&1, format))
+  end
+
+  def puts_one_line_in_columns(fields, format) do
+    :io.format(format, fields)
+  end
 end
